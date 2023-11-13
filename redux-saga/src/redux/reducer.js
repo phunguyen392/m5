de8cc850c0edd8e0e2e1af6a0ad965e48e4aef56
phunguyen392 <nguyenphu.126@gmail.com>
2023-11-13 20:24:22 +0700

    redux

--- redux-saga/src/redux/reducer.js
+++ redux-saga/src/redux/reducer.js
@@ -1,22 +1,11 @@
-import { FETCH_USER_SUCCESS, LOGIN_SUCCESS } from "./action";
 
-// khởi tạo giá trị mặc định cho state gốc.
 
-const initialState = {
-  users: [],
-  userlogined: {}
-};
 
-// Khởi tạo reducer
-const rootReducer = (state = initialState, action) => {
-  // Handle các actions gửi lên
-  switch (action.type) {
-    case LOGIN_SUCCESS:
-      return { ...state, userlogined: action.payload };
-    case FETCH_USER_SUCCESS:
-      return { ...state, users: action.payload };
-    default:
-      return state;
-  }
-};
-export default rootReducer;
+const initialState ={
+    amount : 0
+}
+
+const rootReducer = (initialState = state, action) => {
+    //handle cac action gui len 
+    console.log(state);
+}
